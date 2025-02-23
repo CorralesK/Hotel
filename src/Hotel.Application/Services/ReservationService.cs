00efe6a9bb3a43208bf481be12ebf80d8b63ce1f
dckeisy <keisyangulo15@gmail.com>
2025-02-23 11:43:24 -0600

    feat: a comment is deleted

--- src/Hotel.Application/Services/ReservationService.cs
+++ src/Hotel.Application/Services/ReservationService.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Hotel.src.Hotel.Core.Entities;
+using Hotel.src.Hotel.Core.Enums;
+using Hotel.src.Hotel.Core.Interfaces.IRepository;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,11 +11,46 @@ namespace Hotel.src.Hotel.Application.Services
 {
     class ReservationService
     {
-        reservar
-        cancelar
-        obtener reserva por cliente (idCliente)
+        private readonly IReservationRepository _reservationRepository;
+        private readonly IRoomRepository _roomRepository;
 
-        obtener resevas por rango de fecha
+        //Constructor que recibe el irepositorio como dependencia
+        public ReservationService(IReservationRepository reservationRepository, IRoomRepository roomRepository)
+        {
+            _reservationRepository = reservationRepository;
+            _roomRepository = roomRepository;
+        }
+        // Registrar reserva
+        public void RegisterReservation(Reservation reservation)
+        {
+            reservation.CalculateTotalPrice();
+            _reservationRepository.Add(reservation);
+        }
+
+        // Obtener reserva por cliente(idCliente)
+        public List<Reservation> GetReservationsByClientId(int clientId)
+        {
+            return _reservationRepository.GetByClientId(clientId);
+        }
+
+        // Obtener resevas por rango de fecha
+        public List<Reservation> GetReservationsByDateRange(DateTime startDate, DateTime endDate)
+        {
+            return _reservationRepository.GetByDateRange(startDate, endDate);
+        }
+
+        // Cancelar reserva
+        // Esto me cancela toda la reserva, y lo que dice el enunciado es que se debe cancelar una habitación de la reserva
+        // para liberar una habitación si ya no la necesito
+        public void CancelReservation(long reservationId)
+        {
+            var reservation = _reservationRepository.GetById(reservationId);
+            if (reservation != null)
+            {
+                reservation.STATUS = ReservationStatus.Cancelada;
+                _reservationRepository.Update(reservation);
+            }
+        }
 
     }
 }
