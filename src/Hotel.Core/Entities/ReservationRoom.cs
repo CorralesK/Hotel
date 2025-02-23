f0a8f79c4ddc2ed02a79cf4a41079eb09669c88f
corralesK <kimcorralesv@gmail.com>
2025-02-23 16:09:49 -0600

    Attributes of the “ReservationRoom” entity created.

--- src/Hotel.Core/Entities/ReservationRoom.cs
+++ src/Hotel.Core/Entities/ReservationRoom.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,11 +10,23 @@ namespace Hotel.src.Hotel.Core.Entities
 {
     class ReservationRoom
     {
+        // Hola soy Kim dejo los comentarios en espa;ol para que los entiendas ya que es una llave compuesta.
+
+        [Key]
+        [Column(Order = 1)]  // Indica que esta es la primera parte de la clave compuesta
         public int ReservationID { get; set; }
+
+        [Key]
+        [Column(Order = 2)]  // Indica que esta es la segunda parte de la clave compuesta
+        public int RoomID { get; set; }
+
+        [Required]
         public Reservation Reservation { get; set; }
 
-        public int RoomID { get; set; }
+        [Required]
         public Room Room { get; set; }
+
+        public ReservationRoom() { }
 
     }
 }
