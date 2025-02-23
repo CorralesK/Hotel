a22c16bbb1bd24e92f0a7d66dd8014191cdba260
corralesK <kimcorralesv@gmail.com>
2025-02-23 14:14:00 -0600

    Home screen created.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Hotel.src.Hotel.ConsoleUI;
+using System;
+using Hotel.src.Hotel.ConsoleUI;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Title = "Sistema de Gestión de Reservas de Hotel";
+        ShowStartScreen();
+    }
+
+    static void ShowStartScreen()
+    {
+        Console.Clear();
+        Console.WriteLine("=========================================");
+        Console.WriteLine("     SISTEMA DE RESERVAS DE HOTEL        ");
+        Console.WriteLine("=========================================");
+        Console.WriteLine("\n1. Iniciar sesión\n2. Salir");
+        Console.Write("Seleccione una opción: ");
+        string option = Console.ReadLine();
+
+        switch (option)
+        {
+            case "1":
+                //Aqui se llama el metodo que muestra ek login pero lo voy a usar pata probar mis funcionalidades.
+                break;
+            case "2":
+                Console.WriteLine("Saliendo del sistema...\n");
+                Environment.Exit(0);
+                break;
+            default:
+                Console.WriteLine("\nOpción inválida. Intente de nuevo.");
+                Console.ReadKey();
+                ShowStartScreen();
+                break;
+        }
+    }
+}
