612637595ec52c55eacd4dcc29ee78e65ad47b8a
dckeisy <keisyangulo15@gmail.com>
2025-02-23 11:11:49 -0600

    feat: An intermediate entity is created to be able to manage the many-to-many relationship between rooms and reservations. -In addition, changes are made so that a field is required. -The attribute is then changed to "long" type so that it is bigint.

--- src/Hotel.Core/Entities/Reservation.cs
+++ src/Hotel.Core/Entities/Reservation.cs
@@ -1,6 +1,8 @@
 ﻿using Hotel.src.Hotel.Core.Enums;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,24 +11,32 @@ namespace Hotel.src.Hotel.Core.Entities
 {
     class Reservation
     {
-        public int ID { get; set; }
+        [Key]
+        public long ID { get; set; }
+        [ForeignKey("Customer")]
+        [Required]
         public int CLIENTID { get; set; }
+        [Required]
         public DateTime STARTDATE { get; set; }
+        [Required]
         public DateTime ENDDATE { get; set; }
-        public List<Room> Rooms { get; set; }
+        [Required]
         public float TOTALPRICE {get; set; }
+        [Required]
         public ReservationStatus STATUS { get; set; }
+
+        // Relación muchos a muchos con la tabla de habitaciones (a través de la tabla intermedia ReservationRoom)
+        public ICollection<ReservationRoom> ReservationRooms { get; set; } = new List<ReservationRoom>();
         public Reservation()
         {
-            Rooms = new List<Room>();
+            
         }
-        public Reservation(int id, int clientId, DateTime startDate, DateTime endDate, List<Room> rooms, float totalPrice, ReservationStatus status)
+        public Reservation(int id, int clientId, DateTime startDate, DateTime endDate, float totalPrice, ReservationStatus status)
         {
             ID = id;
             CLIENTID = clientId;
             STARTDATE = startDate;
             ENDDATE = endDate;
-            Rooms = rooms;
             TOTALPRICE = totalPrice;
             STATUS = status;
         }
