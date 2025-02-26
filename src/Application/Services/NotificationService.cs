d977179100b4478986fb34ad4282ba101caab7b8
jojuvaco <josejuvaco@gmail.com>
2025-02-26 17:33:03 -0600

    Add Notification Service

--- src/Application/Services/NotificationService.cs
+++ src/Application/Services/NotificationService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Hotel.src.Core.Interfaces.IServices;
+using Hotel.src.Core.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,34 @@ using System.Threading.Tasks;
 
 namespace Hotel.src.Application.Services
 {
-    class NotificationService
+    public class NotificationService : INotificationService
     {
+        private readonly INotificationSender _sender;
+
+        public NotificationService(INotificationSender sender)
+        {
+            _sender = sender;
+        }
+
+        public void SendCheckInNotification(string recipientEmail, string recipientName, DateTime checkInDate, string roomDetails)
+        {
+            string subject = "Recordatorio de su próxima reserva";
+
+            string message = $@"
+            <html>
+            <body>
+                <h2>Recordatorio de Check-In</h2>
+                <p>Estimado/a {recipientName},</p>
+                <p>Le recordamos que su check-in está programado para el <strong>{checkInDate.ToShortDateString()}</strong>.</p>
+                <p>Detalles de su habitación: {roomDetails}</p>
+                <p>Por favor, asegúrese de tener a mano su identificación y la tarjeta utilizada para la reserva.</p>
+                <p>Si necesita modificar su reserva, contáctenos lo antes posible.</p>
+                <p>¡Esperamos darle la bienvenida pronto!</p>
+                <p>Atentamente,<br>El equipo del Hotel</p>
+            </body>
+            </html>";
+
+            _sender.Send(subject, message, recipientEmail);
+        }
     }
 }
