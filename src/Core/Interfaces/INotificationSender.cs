c625eb27514ed2e11874220d1f902ef5ca74b417
jojuvaco <josejuvaco@gmail.com>
2025-02-26 17:15:13 -0600

    Add Notification Interfaces

---
+++ src/Core/Interfaces/INotificationSender.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel.src.Core.Interfaces
+{
+    public interface INotificationSender
+    {
+        bool Send(string subject, string message, string recipient);
+    }
+}
