eb08ebb2e78afc7934d794ee7aaea8d225c983e7
ACken <kendallangulo2@gmail.com>
2025-02-27 11:47:39 -0600

    Add a new Interface  to register a client

---
+++ src/Core/Interfaces/IServices/IRegisterServices.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Hotel.src.Core.Entities;
+
+namespace Hotel.src.Core.Interfaces.IServices
+{
+    public interface IRegisterServices
+    {
+        User RegisterCustomer(User user);
+    }
+}
