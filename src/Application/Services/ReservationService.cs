7eef5164c5fc8820474e5f938abfb48e0595ff27
dckeisy <keisyangulo15@gmail.com>
2025-02-28 11:54:06 -0600

    feat: Insert fuction to list active reservation

--- src/Application/Services/ReservationService.cs
+++ src/Application/Services/ReservationService.cs
@@ -36,6 +36,12 @@ namespace Hotel.src.Application.Services
         {
             return _reservationRepository.GetByDateRange(startDate, endDate);
         }
+        public List<Reservation> GetActiveReservationsByClientId(int clientId)
+        {
+            return _reservationRepository.GetByClientId(clientId)
+                .Where(r => r.STATUS == ReservationStatus.Confirmada)
+                .ToList();
+        }
 
         // Cancelar habitación de una reserva
         public void CancelRoomInReservation(int reservationId, int roomId)
@@ -48,6 +54,13 @@ namespace Hotel.src.Application.Services
                 {
                     reservation.ReservationRooms.Remove(roomToRemove);
                     reservation.CalculateTotalPrice();
+
+                    // Si ya no tiene habitaciones, cancelar toda la reserva
+                    if (!reservation.ReservationRooms.Any())
+                    {
+                        reservation.STATUS = ReservationStatus.Cancelada;
+                    }
+
                     _reservationRepository.Update(reservation);
                 }
             }
