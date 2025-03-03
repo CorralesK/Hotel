c532c34d747b2b7d88d081758e963884b1354570
jojuvaco <josejuvaco@gmail.com>
2025-03-03 04:35:45 -0600

    Delete file

---
+++ src/Core/Entities/OccupancyReport.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel.src.Core.Entities
+{
+public class OccupancyReport
+    {
+        public int TotalRooms { get; set; }
+        public decimal TotalIncome { get; set; }
+        public decimal OccupancyRate { get; set; }
+        public List<ReportByType> OccupancyByType { get; set; } = new List<ReportByType>();
+        public List<DailyReport> DailyOccupancy { get; set; } = new List<DailyReport>();
+
+        public class ReportByType
+        {
+            public string RoomType { get; set; }
+            public int ReservationsCount { get; set; }
+            public int OccupiedDays { get; set; }
+            public int AvailableDays { get; set; }
+            public decimal OccupancyRateType { get; set; }
+        }
+
+        public class DailyReport
+        {
+            public DateTime Day { get; set; }
+            public int OccupiedRooms { get; set; }
+            public int TotalRooms { get; set; }
+            public decimal OccupancyRateDay { get; set; }
+        }
+    }
+}
